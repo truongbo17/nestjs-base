8a78fc82e47ba82b75ded31b825a77e611dadd02
truongbo <truongnq017@gmail.com>
2024-11-16 23:14:49 +0700

    config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './config/config.type';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app: INestApplication<any> = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  const configService: ConfigService<AllConfigType> = app.get(
+    ConfigService<AllConfigType>,
+  );
+
+  await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
+
 void bootstrap();
