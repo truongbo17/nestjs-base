b3a1df71a362295c66bdadda46f4420332cf70e5
truongbo <truongnq017@gmail.com>
2024-11-19 15:47:58 +0700

    fix config file

--- src/core/files/file.module.ts
+++ src/core/files/file.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import databaseConfig from '../../config/database.config';
+import { DatabaseConfig } from '../../config/database.config.type';
+
+const infrastructurePersistenceModule = (databaseConfig() as DatabaseConfig)
+  .isDocumentDatabase
+  ? ''
+  : '';
 
 @Module({
   imports: [],
