7d4372d5ef81bee5dcec8b455f5a7c1dd88055ad
truongbo <truongnq017@gmail.com>
2024-11-22 21:28:44 +0700

    smooth app main

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ import { join } from 'node:path';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import * as process from 'node:process';
 import { Environment } from './config/app.config';
+import compression from 'compression';
+import { useContainer } from 'class-validator';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   // Run HTTP server
@@ -21,11 +24,49 @@ async function bootstrap() {
     ConfigService<AllConfigType>
   );
 
+  // Logger
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
   process.env.NODE_ENV = configService.getOrThrow('app.appEnv', {
     infer: true,
   });
 
   process.env.TZ = configService.getOrThrow('app.timezone', { infer: true });
+
+  // Compression
+  app.use(compression());
+
+  // Global prefix
+  const globalPrefix = configService.get('app.apiPrefix', { infer: true });
+  if (globalPrefix) {
+    app.setGlobalPrefix(globalPrefix);
+  }
+
+  // For Custom Validation
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  // Versioning
+  const versionEnable: boolean = configService.get<boolean>(
+    'app.urlVersion.enable',
+    { infer: true }
+  );
+  if (versionEnable) {
+    const versioningPrefix: string = configService.getOrThrow(
+      'app.urlVersion.prefix',
+      { infer: true }
+    );
+    const version: number = <number>configService.getOrThrow(
+      'app.urlVersion.version',
+      {
+        infer: true,
+      }
+    );
+    app.enableVersioning({
+      type: VersioningType.URI,
+      defaultVersion: `${version}`,
+      prefix: versioningPrefix,
+    });
+  }
 
   // Views
   app.useStaticAssets(
@@ -44,16 +85,25 @@ async function bootstrap() {
   );
   app.setViewEngine(configService.getOrThrow('view.engine', { infer: true }));
 
-  // Logger
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-
   if (process.env.NODE_ENV !== Environment.PRODUCTION) {
     // Swagger
     const options = new DocumentBuilder()
       .setTitle('API')
       .setDescription('API docs')
       .setVersion('1.0')
-      .addBearerAuth()
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'accessToken'
+      )
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'refreshToken'
+      )
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'google'
+      )
+      .addApiKey({ type: 'apiKey', in: 'header', name: 'x-api-key' }, 'xApiKey')
       .build();
 
     const document = SwaggerModule.createDocument(app, options);
